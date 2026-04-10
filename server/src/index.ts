3f58cc356a75e89d2e75d2bb8c166452fde60522
Parshant Vardhan <parshantvardhan8@gmail.com>
2026-04-10 17:27:04 +0530

    Fix CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
     credentials: true,
   })
 );
